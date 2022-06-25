3a55553a589ef0de9c401dfb6bbdfdef75d4c8c7
Bram Wieringa <b.wieringa31@gmail.com>
2022-06-25 11:47:33 +0200

    Add basic add Node functions

--- src/propertyGraphEntities/propertyGraph.ts
+++ src/propertyGraphEntities/propertyGraph.ts
@@ -1,16 +1,26 @@
 import { Id } from "types/id";
+import { Workload } from "types/workload";
 import { PropertyGraphEdge } from "./propertyGraphEdge";
 import { PropertyGraphNode } from "./propertyGraphNode";
 
 abstract class PropertyGraph {
-    protected static _instance: PropertyGraph;
     nodes: Map<Id, PropertyGraphNode>;
     edges: Map<Id, PropertyGraphEdge>;
 
     protected constructor() {};
+
+    public addNode(node: PropertyGraphNode) {
+        this.nodes.set(node.id, node);
+    }
+
+    public addEdge(edge: PropertyGraphEdge) {
+        this.edges.set(edge.id, edge);
+    }
 }
 
 export class DataPropertyGraph extends PropertyGraph {
+    private static _instance: DataPropertyGraph;
+
     public static get instance(): DataPropertyGraph {
         if (!DataPropertyGraph._instance) {
             DataPropertyGraph._instance = new DataPropertyGraph();
@@ -18,9 +28,14 @@ export class DataPropertyGraph extends PropertyGraph {
 
         return DataPropertyGraph._instance;
     }
+    public induceWorkload(workload: Workload) {
+        return;
+    }
 }
 
 export class InducedPropertyGraph extends PropertyGraph {
+    private static _instance: InducedPropertyGraph;
+
     public static get instance(): InducedPropertyGraph {
         if (!InducedPropertyGraph._instance) {
             InducedPropertyGraph._instance = new InducedPropertyGraph();
@@ -31,6 +46,8 @@ export class InducedPropertyGraph extends PropertyGraph {
 }
 
 export class SchemaPropertyGraph extends PropertyGraph {
+    private static _instance: SchemaPropertyGraph;
+
     public static get instance(): SchemaPropertyGraph {
         if (!SchemaPropertyGraph._instance) {
             SchemaPropertyGraph._instance = new SchemaPropertyGraph();
--- src/simpleGraphEntities/simpleGraph.ts
+++ src/simpleGraphEntities/simpleGraph.ts
@@ -1,20 +1,38 @@
 import { Id } from "types/id";
+import { NodeType } from "types/simpleGraph/nodeType";
 import { SimpleGraphEdgeNode } from "./simpleGraphEdgeNode";
 import { SimpleGraphLabelNode } from "./simpleGraphLabelNode";
 import { SimpleGraphNodeNode } from "./simpleGraphNodeNode";
 import { SimpleGraphPropertyNode } from "./simpleGraphPropertyNode";
 
 abstract class SimpleGraph {
-    protected static _instance: SimpleGraph;
-    nodes: Map<Id, SimpleGraphNodeNode>;
-    edges: Map<Id, SimpleGraphEdgeNode>;
-    labels: Map<Id, SimpleGraphLabelNode>;
-    properties: Map<Id, SimpleGraphPropertyNode>;
+    nodes: Map<`${NodeType}_${Id}`, SimpleGraphNodeNode>;
+    edges: Map<`${NodeType}_${Id}`, SimpleGraphEdgeNode>;
+    labels: Map<`${NodeType}_${Id}`, SimpleGraphLabelNode>;
+    properties: Map<`${NodeType}_${Id}`, SimpleGraphPropertyNode>;
 
     protected constructor() {};
+
+    public addNode(node: SimpleGraphNodeNode) {
+        this.nodes.set(`${node.nodeType}_${node.nodeId}`, node)
+    }
+
+    public addEdge(node: SimpleGraphEdgeNode) {
+        this.edges.set(`${node.nodeType}_${node.nodeId}`, node)
+    }
+
+    public addLabel(node: SimpleGraphLabelNode) {
+        this.labels.set(`${node.nodeType}_${node.nodeId}`, node)
+    }
+
+    public addProperty(node: SimpleGraphPropertyNode) {
+        this.properties.set(`${node.nodeType}_${node.nodeId}`, node)
+    }
 }
 
 export class DataSimpleGraph extends SimpleGraph {
+    private static _instance: DataSimpleGraph;
+
     public static get instance(): DataSimpleGraph {
         if (!DataSimpleGraph._instance) {
             DataSimpleGraph._instance = new DataSimpleGraph();
@@ -25,6 +43,8 @@ export class DataSimpleGraph extends SimpleGraph {
 }
 
 export class InducedSimpleGraph extends SimpleGraph {
+    private static _instance: InducedSimpleGraph;
+
     public static get instance(): InducedSimpleGraph {
         if (!InducedSimpleGraph._instance) {
             InducedSimpleGraph._instance = new InducedSimpleGraph();
@@ -35,6 +55,8 @@ export class InducedSimpleGraph extends SimpleGraph {
 }
 
 export class SchemaSimpleGraph extends SimpleGraph {
+    private static _instance: SchemaSimpleGraph;
+
     public static get instance(): SchemaSimpleGraph {
         if (!SchemaSimpleGraph._instance) {
             SchemaSimpleGraph._instance = new SchemaSimpleGraph();
