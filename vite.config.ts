a90b75c336843c41189e419a5eb49129e8ad27eb
Bram Wieringa <b.wieringa31@gmail.com>
2022-08-30 21:52:06 +0200

    Fix styling

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import postcss from './postcss.config.cjs';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), tsconfigPaths()]
+  plugins: [svelte(), tsconfigPaths()],
+  css:{
+    postcss
+  }
 })
